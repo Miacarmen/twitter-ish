81d8622409f42dc6f7e9c36386a80537184871ee
miacarmen <maitreyacarmen@gmail.com>
2022-05-20 19:24:07 -0700

    added db connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+// require mongoose
+const { connect, connection } = require('mongoose');
+
+const connectionString = 
+process.env.MONGODB_URI || 'mongodb://localhost:27017/twitter-ishDB';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;
