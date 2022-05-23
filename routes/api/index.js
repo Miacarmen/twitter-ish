a0871abd321f2245741e44430d000ffc7e911265
miacarmen <maitreyacarmen@gmail.com>
2022-05-22 20:10:14 -0700

    added env file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+
+// import routes
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
+// endpoints
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+// import api routes
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
