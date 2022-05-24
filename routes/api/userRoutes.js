6cf181ac0144ae8162105c76123c192b2b9ea06e
miacarmen <maitreyacarmen@gmail.com>
2022-05-23 18:48:58 -0700

    add or delete friend routes added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,20 +6,29 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    addFriend, 
+    deleteFriend,
 } = require('../../controllers/userController');
 
-// Users Routes
+// Route to get all users or create a user
 router
     .route('/')
     .get(getUsers)
     .post(createUser);
 
+// route to get user by id, update, or delete
 router
     .route('/:id')
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser);
 
-// TO-DO: routes to add or remove friends
+// route to add a friend
+router.route('/:userId/friends')
+.post(addFriend);
+
+// route to delete a friend
+router.route('/:uderId/friends/:friendId')
+    .delete(deleteFriend);
 
 module.exports = router;
